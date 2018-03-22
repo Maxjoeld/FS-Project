0fed8e5c412ec77ed460a290a30194d465eb0313
Maximo Delarosa <mjd809@gmail.com>
2018-03-22 00:41:32 -0400

    Wired front end with backend
    still have issues aquiring a connection

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,18 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import AccountCreate from "./AccountCreate";
 
 class App extends Component {
+  state = {
+    login: false,
+    createAccount: true,
+  };
+
+  togglelogin = (componentName) => {
+    if (componentName === 'createAccount') {
+      this.setState({ login: true, createAccount: false });
+    }
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <header className="App-header" />
+        {this.state.login ? <div>Login</div> : null}
+        {this.state.createAccount ? <AccountCreate togglelogin={this.togglelogin}/> : null}
       </div>
     );
   }
